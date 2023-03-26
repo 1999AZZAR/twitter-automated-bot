6ac77d3d969c2b0532a3813b196623dd6934a80d
azzar <azzar.mr.zs@gmail.com>
2023-03-26 12:25:17 +0700

    Update retweet.py

--- retweet.py
+++ retweet.py
@@ -1,23 +1,22 @@
 import tweepy
-from time import sleep
+import time
 from twitter import Twitter
-from tweepy import api
 from constants import *
 
+# Authenticate with Twitter API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SCRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 tw = Twitter()
 
-for tweet in tweepy.Cursor(api.search,q=('#lamongan' or '#lamonganmegilan' or '#beritalamongan'),).items(20):
+# Search for tweets and retweet them
+for tweet in tweepy.Cursor(api.search, q='#lamongan OR #lamonganmegilan OR #beritalamongan').items(20):
     try:
         print('\nbot found tweet by @' + tweet.user.screen_name)
         tweet.retweet()
-        print('retweet successfully.')
-        sleep(25)
+        print('retweet successful.')
+        time.sleep(25) # Wait for 25 seconds before retweeting the next tweet
     except tweepy.TweepError as error:
-        print('\nError. Retweet not successful. Reason: ')
-        print(error.reason)
-    except StopIteration:
-        break
+        print('\nError: Retweet not successful. Reason: ' + error.reason)
+        continue

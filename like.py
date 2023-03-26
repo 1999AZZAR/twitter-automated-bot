4a879c44125ed6c54cf2399d5c640c2011ab18ce
azzar <azzar.mr.zs@gmail.com>
2023-03-26 12:19:45 +0700

    Update like.py

--- like.py
+++ like.py
@@ -1,7 +1,6 @@
 import tweepy
 from time import sleep
 from twitter import Twitter
-from tweepy import api
 from constants import *
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SCRET)
@@ -10,11 +9,14 @@ api = tweepy.API(auth)
 
 tw = Twitter()
 
-for tweet in tweepy.Cursor(api.search,q=('#lamongan' or '#lamonganmegilan' or '#beritalamongan'),).items(10):
+search_query = '#lamongan OR #lamonganmegilan OR #beritalamongan'
+max_tweets = 10
+
+for tweet in tweepy.Cursor(api.search, q=search_query).items(max_tweets):
     try:
         print('\nTweet by: @' + tweet.user.screen_name)
-        tweet.favorite()
-        print('Like the tweet')
+        api.create_favorite(tweet.id)
+        print('Liked the tweet')
         sleep(25)
     except tweepy.TweepError as e:
         print(e.reason)
